d92f8271ae77fe39f0c203a6c5b4555b68e4cfc4
yukitoohard <564144170@qq.com>
2022-03-14 19:26:33 +0800

    中心扩散法提交

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -11,8 +11,9 @@
  */
 
 var longestPalindrome = function(s) {
+    // 方法一：动态规划
+    /* 
     let len = s.length;
-    // if (len < 2) return s;
 
     let longestPalindromeLen = 0, longestPalindromeStr = "";
     let dp = new Array(len).fill(0).map(() => new Array(len).fill(false));
@@ -26,6 +27,9 @@ var longestPalindrome = function(s) {
         }
     }
 
+    // 状态转移方程dp[i][j] = dp[i+1][j-1] && s[i] === s[j]
+    // 对于所有的i === j，dp[i][j] = true
+    // 对于所有的i !== j，dp[i][j] = false
     for (let j=0; j<len; j++) {
         dp[j][j] = true;
         updatePalindrome(j, j);
@@ -42,6 +46,28 @@ var longestPalindrome = function(s) {
     }
 
     return longestPalindromeStr;
+    */
+
+    // 方法二：中心扩散
+    let res = "";
+    for (let i=0; i<s.length; i++) {
+        // 处理奇数长度
+        const s1 = palindrome(s, i, i);
+        // 处理偶数长度
+        const s2 = palindrome(s, i, i+1);
+
+        res = res.length < s1.length ? s1 : res;
+        res = res.length < s2.length ? s2 : res;
+    }
+    return res;
+    // 返回以l，r为中心点的最长回文串
+    function palindrome (s, l, r) {
+        while (l >= 0 && r < s.length && s[l] === s[r]) {
+            l--;
+            r++;
+        }
+        return s.slice(l+1, r);
+    }
 };
 
 // @lc code=end
