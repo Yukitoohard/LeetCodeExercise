370060a35cd3855e87f9e7400c5fdd6a91023745
YukitooHard <564144170@qq.com>
2022-04-09 16:19:30 +0800

    递归

---
+++ 104.二叉树的最大深度.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+    let left_Height = maxDepth(root.left);
+    let right_Height = maxDepth(root.right);
+    return Math.max(left_Height, right_Height) + 1;
+};
+// @lc code=end
+
