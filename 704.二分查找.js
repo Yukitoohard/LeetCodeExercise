cf20b2535080bdf52b3f3735e1adae75b6e28e28
YukitooHard <564144170@qq.com>
2022-05-20 11:37:38 +0800

    数组二分查找

--- 704.二分查找.js
+++ 704.二分查找.js
@@ -10,16 +10,21 @@
  * @param {number} target
  * @return {number}
  */
+
+// 二分查找前提：1）有序数组；2）无重复项
 var search = function(nums, target) {
     // return nums.indexOf(target);
+    // 左闭右闭区间
     let size = nums.length;
     let left = 0, right = size - 1;
     while (left <= right) {
-        let mid = left + Math.floor((right - left) / 2);
+        let mid = left + Math.floor((right - left) / 2);  // 防止溢出，等同于(left + right)/2
         if (target < nums[mid]) {
-            right = mid -1;
+            // target在左区间
+            right = mid -1;  
         }
         else if (target > nums[mid]) {
+            // target在右区间
             left = mid + 1;
         }
         else {
